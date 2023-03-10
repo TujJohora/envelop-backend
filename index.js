6a51bb932058e0b785c85aaa8b12805b9de3ad7e
Fatema Tuj Johora <tujjohoraa@gmail.com>
2023-03-10 18:48:42 +0600

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
+
 const express = require("express");
 const cors= require("cors");
+const mongoose = require("mongoose");
 
 // making express app
 const app = express();
@@ -8,7 +12,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//port
+const PORT = process.env.PORT|| 4000;
+
+//connect to database
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
 //Listening for requests
 app.listen(PORT, (req,res)=>{
-
+    console.log(`Connected to db & Server running on port: ${PORT}`);
+});
 })
+.catch(err =>{
+    console.log(err.message);
+});
+
